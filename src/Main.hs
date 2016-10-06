9444552354193a17eb07fcd0128efaf933fd5e1a
Juan C. Montemayor E <j.mont@me.com>
2016-10-06 19:52:57 -0400

    Read in, compile and print out csv of members

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,29 @@
 
 module Main (main) where
 
-import System.Environment (getArgs)
+import Data.List.Split
+
+-- Date,Last Name,First Name,Email Address,Status
+
+data AttendanceStatus = Going | NotGoing deriving (Show)
+
+data Member = Member {
+    date :: String,
+    lastName :: String,
+    firstName :: String,
+    email :: String,
+    status :: AttendanceStatus
+} deriving (Show)
+
+mkMember :: String -> Member
+mkMember row =
+    let [date, last, first, email, status] = splitOn "," row
+        attending = if status == "Attending" then Going else NotGoing
+     in Member date last first email attending
 
 main = do
-    args <- getArgs
-    putStrLn "Hello World"
-    mapM putStrLn args
+    sampleCSVFile <- readFile "src/data/orgSyncSample.csv"
+    let (header:rows) = lines sampleCSVFile
+    let members = map mkMember rows
+    putStrLn $ unlines $ map show members
     return ()
